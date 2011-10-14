8aee7d35ef9769ba81637caaa715d0af4548dcca
Luke Mayes <lukemayes@gmail.com>
2011-10-13 21:41:58 -0500

    Added template user.js that includes latest version of jQuery.

---
+++ jquery.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         jQuery For Chrome (A Cross Browser Example)
+// @namespace    jQueryForChromeExample
+// @include      *
+// @author       Erik Vergobbi Vold
+// @description  This userscript is meant to be an example on how to use jQuery in a userscript on Google Chrome.
+// ==/UserScript==
+
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+function addJQuery(callback) {
+	var script = document.createElement("script");
+	script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+	script.addEventListener('load', function() {
+		var script = document.createElement("script");
+	    script.textContent = "(" + callback.toString() + ")();";
+	    document.body.appendChild(script);
+	}, false);
+	document.body.appendChild(script);
+}
+
+// the guts of this userscript
+function main() {
+	$('code').load(function() {
+		console.log("code is loaded");
+	});
+	$('code').each(function(index, it) {
+		console.log(it);
+	});
+}
+
+// load jQuery and execute the main function
+addJQuery(main);
